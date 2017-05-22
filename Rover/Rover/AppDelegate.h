5d8177836e65a40899872f6505875034dc9a3266
Ryan <ryanplitt@gmail.com>
2017-05-22 14:07:21 -0600

    Initial Commit

---
+++ Rover/Rover/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Rover
+//
+//  Created by Ryan Plitt on 5/22/17.
+//  Copyright © 2017 Ryan Plitt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Rover/Rover/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Rover
+//
+//  Created by Ryan Plitt on 5/22/17.
+//  Copyright © 2017 Ryan Plitt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
